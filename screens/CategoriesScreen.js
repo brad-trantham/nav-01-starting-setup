52eecf2390b9dc5858cdedd87233aaefc4348126
Brad Trantham <btrantham@swri.org>
2020-04-03 16:52:22 -0500

    Commit after lecture 114

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, Button, StyleSheet} from 'react-native'
 
 const CategoriesScreen = props => {
     return(
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
+            <Button title="Go to Meals!" onPress={()=>{
+                // alternatively props.navigation.navigate('CategoryMeals')
+                props.navigation.navigate({routeName: 'CategoryMeals'})
+            }} />
         </View>
     )
 }
