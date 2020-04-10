cf1467dbaaa0c477f8aa27b964d419cd631e471a
Brad Trantham <btrantham@swri.org>
2020-04-10 11:50:55 -0500

    Commit after lecture 119

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
+import {View, Text, FlatList, StyleSheet, TouchableOpacity, Platform} from 'react-native'
+
+import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors'
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return(
+               <TouchableOpacity style={styles.gridItem} onPress={() => {
+                    props.navigation.navigate({routeName: 'CategoryMeals',
+                         params: {
+                             categoryId: itemData.item.id
+                         }})              
+               }}>
+                    <View>
+                        <Text>{itemData.item.title}</Text>
+                    </View>
+               </TouchableOpacity> 
+        )
+    }
+
     return(
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to Meals!" onPress={()=>{
-                // alternatively props.navigation.navigate('CategoryMeals')
-                //
-                // you can also use navigation.replace() to change the screen,
-                // but this replaces the root of the stack
-                // Sometimes this is handy, like when you have a login
-                // screen that you don't want to go back to
-                props.navigation.navigate({routeName: 'CategoryMeals'})
-            }} />
-        </View>
+        // the flatlist also needs a keyextractor in older versions of RN,
+        // but newer versions will detect 'id' as the key
+        <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
     )
+}
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
 }
 
 const styles = StyleSheet.create({
@@ -23,6 +40,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 })
 
