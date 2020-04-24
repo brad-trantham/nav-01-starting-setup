367e21fe65986dc7f247f6c84c0405ed6d2edbda
Brad Trantham <btrantham@swri.org>
2020-04-24 15:59:39 -0500

    Fixed that bug with font async loading

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer)
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   })
